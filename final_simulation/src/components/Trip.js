561bf0cd75dba1e325dff2b908bbe36803a726ff
doyeonryu <doyeonryu.dyr@gmail.com>
2024-11-25 01:46:09 +0900

    modified foot data

--- final_simulation/src/components/Trip.js
+++ final_simulation/src/components/Trip.js
@@ -76,7 +76,7 @@ const ICON_MAPPING = {
 // mapbox 스타일 변경
 // 최소, 최대 시간 설정 (min ~ max)
 const minTime = 600; // min
-const maxTime = 1090; // max
+const maxTime = 1095; // max
 
 const animationSpeed = 0.1;
 const mapStyle = "mapbox://styles/doyeonryu/clxa4e4wo01ox01rb7yhw4pg3";
